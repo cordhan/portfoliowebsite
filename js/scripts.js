d31e07ebb11df22ee11832e6f1f3d6d82f6a3b54
cordhan <cordula@cordhan.net>
2019-01-26 20:46:24 +0000

    Implemented hamburger menu and dropdown for mobile screens.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,20 @@ $(document).ready(function() {
     $(this).siblings(".project-details").slideToggle();
     });
 });
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.menu')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
