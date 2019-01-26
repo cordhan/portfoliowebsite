1a988039758e0368f572d2fffe708618f8263c0b
cordhan <cordula@cordhan.net>
2019-01-26 17:51:33 +0000

    Added interactive project details to index page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 console.log("Welcome to CordHan UX!")
+
+$(document).ready(function() {
+  $(".project-preview").on("click", function() {
+    $(this).siblings(".project-details").slideToggle();
+    });
+});
